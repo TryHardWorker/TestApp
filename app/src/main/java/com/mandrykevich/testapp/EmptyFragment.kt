e4f405372a59822719c482292eb238a850caf561
Nick <werilju@gmail.com>
2024-09-29 16:40:31 +0300

    fragments added

---
+++ app/src/main/java/com/mandrykevich/testapp/EmptyFragment.kt
@@ -0,0 +1,29 @@
+package com.mandrykevich.testapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class EmptyFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_empty, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = EmptyFragment()
+    }
+
+}
